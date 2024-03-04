7e5739ddda1a3b05a98e226d83d0e167109ffb20
Elena Lem <151932734+Leksa2304@users.noreply.github.com>
2024-03-04 20:01:38 +0300

    Д/з -5.  Генератор предсказаний
    При click на button проверяет есть ли текущее предсказание-прогноз.  нет - создает новое,
    есть - добавляет предыдущее в список моих предсказаний и генерирует новое с % вероятности

--- script.js
+++ script.js
@@ -13,3 +13,58 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const predictionButton = document.querySelector("button");
+const currentForecastTitle = document.querySelector(".current-forecast h1");
+const currentForecastProbability = document.querySelector(".current-forecast p");
+
+// Функция-генератор числа и процента вероятности
+function generationDate(min, max) {
+    const generationNumber = Math.round(Math.random() * (max - min) + min);
+    return generationNumber;
+}
+
+// Клик по кнопке
+predictionButton.addEventListener("click", function() {
+
+    makeListForecast();
+
+    const generationNumber = generationDate(1, 10);
+    const predictionPercent = generationDate(0, 100);
+
+    let textPrediction;
+
+    if (generationNumber == 1) {
+        textPrediction = "Ты на верном пути!";
+    } else if (generationNumber == 2) {
+        textPrediction = "У тебя все получится!";
+    } else if (generationNumber == 3) {
+        textPrediction = "Ты устроишься на работу своей мечты!";
+    } else if (generationNumber == 4) {
+        textPrediction = "Тебя ждет незабываемый отпуск!";
+    } else {
+        textPrediction = "Тебе повезет!";
+    }
+
+    currentForecastTitle.textContent = textPrediction;
+    currentForecastProbability.textContent = `Вероятность: ${predictionPercent}%`;
+});
+
+// Создание моего списка предсказаний
+function makeListForecast() {
+
+    const title = currentForecastTitle.textContent;
+    const percent = currentForecastProbability.textContent;
+
+    if (!title && !percent) {
+        return;
+    }
+
+    const forecasts = document.querySelector(".forecasts");
+    const templateForecast = document.querySelector("#forecast-item");
+    const myForecast = templateForecast.content.cloneNode(true);
+
+    myForecast.querySelector("h3").textContent = title;
+    myForecast.querySelector("p").textContent = `Вероятность: ${percent}%`;
+    forecasts.prepend(myForecast);
+};
